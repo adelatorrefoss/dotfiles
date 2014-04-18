ce633bfbe0984d580a6e58c473cf7915b367689f
Antonio de la Torre <antonio.delatorre@kaleidos.net>
2014-04-18 13:51:47 +0200

    fist push

---
+++ create-links.sh
@@ -0,0 +1,15 @@
+# create links to config files
+
+cd ~
+ln -fs bin/dotfiles/awesome
+ln -fs bin/dotfiles/.bash_aliases
+ln -fs bin/dotfiles/.bashrc
+ln -fs bin/dotfiles/.ctags
+ln -fs bin/dotfiles/.git-completion.bash
+ln -fs bin/dotfiles/.gitconfig
+ln -fs bin/dotfiles/.i3/
+ln -fs bin/dotfiles/.profile
+ln -fs bin/dotfiles/.vim/
+ln -fs .vim/vimrc .vimrc
+
+
