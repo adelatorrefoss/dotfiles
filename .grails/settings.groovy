5f14fdc8c6c73c03c10bcfa163dbc5167ce102ea
Antonio de la Torre <antonio.delatorre@kaleidos.net>
2014-10-07 16:19:04 +0200

    create .grails/settings.groovy file

---
+++ .grails/settings.groovy
@@ -0,0 +1,1 @@
+grails.dependency.cache.dir = "target/ivy-cache"
--- create-links.sh
+++ create-links.sh
@@ -1,17 +1,21 @@
 # create links to config files
 
 cd ~
-ln -fs bin/dotfiles/awesome
-ln -fs bin/dotfiles/.bash_aliases
-ln -fs bin/dotfiles/.bashrc
-ln -fs bin/dotfiles/.ctags
-ln -fs bin/dotfiles/.git-completion.bash
-ln -fs bin/dotfiles/.gitconfig
-ln -fs bin/dotfiles/.i3/
-ln -fs bin/dotfiles/.profile
-ln -fs bin/dotfiles/.vim/
+ln -fs ~/bin/dotfiles/awesome
+ln -fs ~/bin/dotfiles/.bash_aliases
+ln -fs ~/bin/dotfiles/.bashrc
+ln -fs ~/bin/dotfiles/.ctags
+ln -fs ~/bin/dotfiles/.git-completion.bash
+ln -fs ~/bin/dotfiles/.gitconfig
+ln -fs ~/bin/dotfiles/.i3/
+ln -fs ~/bin/dotfiles/.profile
+ln -fs ~/bin/dotfiles/.vim/
 ln -fs .vim/vimrc .vimrc
-ln -fs bin/dotfiles/.i3status.conf
-ln -fs bin/dotfiles/.tmux.conf
-ln -fs bin/dotfiles/.zshrc
-ln -fs bin/dotfiles/.oh-my-zsh
+ln -fs ~/bin/dotfiles/.i3status.conf
+ln -fs ~/bin/dotfiles/.tmux.conf
+ln -fs ~/bin/dotfiles/.zshrc
+ln -fs ~/bin/dotfiles/.oh-my-zsh
+mkdir ~/.grails
+cd ~/.grails
+ln -fs ~/bin/dotfiles/.grails/settings.groovy
+cd ~
