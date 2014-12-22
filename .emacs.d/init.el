dcf54601bd907edf93678f04b6d7a03d0270dbb2
Antonio de la Torre <antonio.delatorre@kaleidos.net>
2014-12-22 17:43:10 +0100

    emacs indent 4 spaces, nyan-cat, highlight tabs and move temp files to /tmp

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,6 +6,11 @@
 ;;                 C-p
 ;;   C-a  --- C-b  <|>  C-f ---  C-e
 ;;                 C-n
+;;      M-a     Retroceder al comienzo de la oración
+;;      M-e     Avanzar al final de la oración
+;; C-v  Avanzar una pantalla completa
+;; M-v  Retroceder una pantalla completa
+;; C-l  Limpiar la pantalla y mostrar todo el texto de nuevo,
 
 ;; M-b - Moverse hacia atrás una palabra
 ;; M-f - Moverse hacia adelante una palabra
@@ -14,6 +19,16 @@
 
 ;; M-d - Borrar palabra hacia adelante
 ;; C-<backspace> - Borrar palabra hacia atrás
+
+;; <DEL>        borra el carácter justo antes que el cursor
+;; C-d          borra el siguiente carácter después del cursor
+
+;; M-<DEL>      Elimina la palabra inmediatamente antes del
+;;                  cursor
+;; M-d          Elimina la siguiente palabra después del cursor
+
+;; C-k                  Elimina desde el cursor hasta el fin de la línea
+;; M-k                  Elimina hasta el final de la oración actual
 
 ;; C-x C-s - Guardar cambios
 
@@ -37,6 +52,8 @@
 ;; C-x 1 - Dejar ventana actual
 ;; C-x 2 - Split ventana horizontal
 ;; C-x 3 - Split ventana vertical
+
+;; con C-x C- muestras el directorio del fichero actual
 
 (ido-mode t)
 (ido-everywhere t)
@@ -88,57 +105,57 @@
 
 
 
-;; C-c p f	Display a list of all files in the project. With a prefix argument it will clear the cache first.
-;; C-c p F	Display a list of all files in all known projects.
+;; C-c p f      Display a list of all files in the project. With a prefix argument it will clear the cache first.
+;; C-c p F      Display a list of all files in all known projects.
 
-;; C-c p g	Display a list of all files at point in the project. With a prefix argument it will clear the cache first.
-;; C-c p 4 f	Jump to a project's file using completion and show it in another window.
-;; C-c p 4 g	Jump to a project's file based on context at point and show it in another window.
+;; C-c p g      Display a list of all files at point in the project. With a prefix argument it will clear the cache first.
+;; C-c p 4 f    Jump to a project's file using completion and show it in another window.
+;; C-c p 4 g    Jump to a project's file based on context at point and show it in another window.
 
-;; C-c p d	Display a list of all directories in the project. With a prefix argument it will clear the cache first.
+;; C-c p d      Display a list of all directories in the project. With a prefix argument it will clear the cache first.
 
-;; C-c p 4 d	Switch to a project directory and show it in another window.
-;; C-c p 4 a	Switch between files with the same name but different extensions in other window.
+;; C-c p 4 d    Switch to a project directory and show it in another window.
+;; C-c p 4 a    Switch between files with the same name but different extensions in other window.
 
-;; C-c p T	Display a list of all test files(specs, features, etc) in the project.
-;; C-c p l	Display a list of all files in a directory (that's not necessarily a project)
+;; C-c p T      Display a list of all test files(specs, features, etc) in the project.
+;; C-c p l      Display a list of all files in a directory (that's not necessarily a project)
 
-;; C-c p s g	Run grep on the files in the project.
+;; C-c p s g    Run grep on the files in the project.
 
-;; M-- C-c p s g	Run grep on projectile-grep-default-files in the project.
-;; C-c p v	Run vc-dir on the root directory of the project.
+;; M-- C-c p s g        Run grep on projectile-grep-default-files in the project.
+;; C-c p v      Run vc-dir on the root directory of the project.
 
-;; C-c p b	Display a list of all project buffers currently open.
-;; C-c p 4 b	Switch to a project buffer and show it in another window.
-;; C-c p 4 C-o	Display a project buffer in another window without selecting it.
+;; C-c p b      Display a list of all project buffers currently open.
+;; C-c p 4 b    Switch to a project buffer and show it in another window.
+;; C-c p 4 C-o  Display a project buffer in another window without selecting it.
 
-;; C-c p a	Switch between files with the same name but different extensions.
-;; C-c p o	Runs multi-occur on all project buffers currently open.
+;; C-c p a      Switch between files with the same name but different extensions.
+;; C-c p o      Runs multi-occur on all project buffers currently open.
 
-;; C-c p r	Runs interactive query-replace on all files in the projects.
+;; C-c p r      Runs interactive query-replace on all files in the projects.
 
-;; C-c p i	Invalidates the project cache (if existing).
+;; C-c p i      Invalidates the project cache (if existing).
 
-;; C-c p R	Regenerates the projects TAGS file.
+;; C-c p R      Regenerates the projects TAGS file.
 
-;; C-c p j	Find tag in project's TAGS file.
+;; C-c p j      Find tag in project's TAGS file.
 
-;; C-c p k	Kills all project buffers.
-;; C-c p D	Opens the root of the project in dired.
-;; C-c p e	Shows a list of recently visited project files.
-;; C-c p s a	Runs ack on the project. Requires the presence of ack-and-a-half.
-;; C-c p s s	Runs ag on the project. Requires the presence of ag.el.
-;; C-c p !	Runs shell-command in the root directory of the project.
-;; C-c p &	Runs async-shell-command in the root directory of the project.
-;; C-c p c	Runs a standard compilation command for your type of project.
-;; C-c p P	Runs a standard test command for your type of project.
-;; C-c p t	Toggle between an implementation file and its test file.
-;; C-c p 4 t	Jump to implementation or test file in other window.
-;; C-c p z	Adds the currently visited file to the cache.
-;; C-c p p	Display a list of known projects you can switch to.
-;; C-c p S	Save all project buffers.
-;; C-c p m	Run the commander (an interface to run commands with a single key).
-;; C-c p ESC	Switch to the most recently selected projectile buffer.
+;; C-c p k      Kills all project buffers.
+;; C-c p D      Opens the root of the project in dired.
+;; C-c p e      Shows a list of recently visited project files.
+;; C-c p s a    Runs ack on the project. Requires the presence of ack-and-a-half.
+;; C-c p s s    Runs ag on the project. Requires the presence of ag.el.
+;; C-c p !      Runs shell-command in the root directory of the project.
+;; C-c p &      Runs async-shell-command in the root directory of the project.
+;; C-c p c      Runs a standard compilation command for your type of project.
+;; C-c p P      Runs a standard test command for your type of project.
+;; C-c p t      Toggle between an implementation file and its test file.
+;; C-c p 4 t    Jump to implementation or test file in other window.
+;; C-c p z      Adds the currently visited file to the cache.
+;; C-c p p      Display a list of known projects you can switch to.
+;; C-c p S      Save all project buffers.
+;; C-c p m      Run the commander (an interface to run commands with a single key).
+;; C-c p ESC    Switch to the most recently selected projectile buffer.
 
 
 (defun comment-or-uncomment-block ()
@@ -159,15 +176,37 @@
 (global-unset-key (kbd "C-z"))
 (global-set-key (kbd "C-z C-z") 'comment-or-uncomment-block)
 
-;; activar ggtags-mode (navegar por tags) 
+;; activar ggtags-mode (navegar por tags)
 (add-hook 'prog-mode-hook (lambda ()
-			    (interactive)
-			    (ggtags-mode 1)))
+                            (interactive)
+                            (ggtags-mode 1)))
 (add-hook 'dired-mode-hook (lambda ()
-			     (ggtags-mode 1)))
+                             (ggtags-mode 1)))
 
-;; con C-x C-j muestras el directorio del fichero actual
+;; con C-x C- muestras el directorio del fichero actual
 (require 'dired+)
 
 ;; Mostrar el directorio del proyecto al cambiar de proyecto
 (setq projectile-switch-project-action 'projectile-dired)
+
+;; ag
+(require 'ag)
+
+;; indent with spaces
+(setq-default c-basic-offset 4)
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+
+;; highlight tabs
+(require 'highlight-chars)
+(add-hook 'font-lock-mode-hook 'hc-highlight-tabs)
+
+;; Nyam-mode
+(nyan-mode)
+(nyan-start-animation)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
