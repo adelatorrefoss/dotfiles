9a696d87e19a256df745cd0f87353d2204f836a6
Antonio de la Torre <antonio.delatorre@kaleidos.net>
2015-07-16 14:20:59 +0200

    add kaleidos repo

--- .grails/settings.groovy
+++ .grails/settings.groovy
@@ -1,1 +1,10 @@
 grails.dependency.cache.dir = "target/ivy-cache"
+
+
+grails.project.dependency.authentication = {
+    credentials {
+        id = 'kaleidos'
+        username = 'antonio.delatorre'
+        password = 'APAaXsuHYADUe2VeuTeoNd1Af9z'
+    }
+}
